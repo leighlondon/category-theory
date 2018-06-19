bf6e9fba40c451f686bd9abb53fc086159e6906e
Leigh London <leigh.00.london@gmail.com>
2018-06-19 14:53:18 +1000

    spec the memoize function

--- memoize.spec.js
+++ memoize.spec.js
@@ -4,4 +4,20 @@ describe("memoize", () => {
   it("hands back a function", () => {
     expect(typeof memoize).toBe("function");
   });
+
+  it("executes cached functions faster", () => {
+    const mock = jest.fn(x => x);
+    const memo = memoize(mock);
+    expect(memo(1)).toBe(1);
+    expect(memo(1)).toBe(1);
+    expect(mock).toHaveBeenCalledTimes(1);
+  });
+
+  it("caches independent calls separately", () => {
+    const mock = jest.fn(x => x);
+    const memo = memoize(mock);
+    expect(memo(1)).toBe(1);
+    expect(memo(2)).toBe(2);
+    expect(mock).toHaveBeenCalledTimes(2);
+  });
 });
